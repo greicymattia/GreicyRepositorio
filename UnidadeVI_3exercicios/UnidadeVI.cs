ba7f02ae1cb5b6455fb1c73afa9b47bcc89f07ca
Greicy de Matias Silva <greicy_matias@hotmail.com>
2014-04-27 11:30:22 -0300

    continuação da unidade VI (3 exercicios)

--- UnidadeVI_3exercicios/UnidadeVI.cs
+++ UnidadeVI_3exercicios/UnidadeVI.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UnidadeVI_3exercicios
+namespace UnidadeVI_continuacao_3
 {
     class UnidadeVI
     {
@@ -48,6 +48,60 @@ namespace UnidadeVI_3exercicios
                 }
                 Console.ReadKey();
             }
+        }
+        static void Main9(string[] args)
+        {
+            Console.Write("Digite um valor para A: ");
+            double A = double.Parse(Console.ReadLine());
+            Console.Write("Digite um valor para B: ");
+            double B = double.Parse(Console.ReadLine());
+            Console.Write("Digite um valor para C: ");
+            double C = double.Parse(Console.ReadLine());
+            double soma = A + B;
+            if (soma > C)
+            {
+                Console.WriteLine("Soma de A e B maior que C!");
+            }
+            else if (soma == C)
+            {
+                Console.WriteLine("Soma de A e B igual a C!");
+            }
+            else
+            {
+                Console.WriteLine("Soma de A e B menor que C!");
+            }
+            Console.ReadKey();
+        }
+        static void Main10(string[] args)
+        {
+            string[] nome = new string[4];
+            double[] preco = new double[4];
+            double[] desc = new double[4];
+            int i = 0;
+            double total = 0;
+            for (i = 0; i < 4; i++)
+            {
+                int j = i+1;
+                Console.Write("Digite o nome do artigo " + j + ": ");
+                nome[i] = Console.ReadLine();
+                Console.Write("Digite o preço do artigo " + j + ": ");
+                preco[i] = double.Parse(Console.ReadLine());
+                Console.Write("Digite o percentual de desconto do artigo "+j+": ");
+                desc[i] = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+                desc[i] = preco[i] - ((preco[i] * desc[i]) / 100);
+                total = total + desc[i];
+            }
+            Console.Clear();
+            for(i=0; i<4;i++)
+            {
+                Console.WriteLine("Nome: " + nome[i]);
+                Console.WriteLine("Preço: R$" + preco[i]);
+                Console.WriteLine("Valor com desconto: R$" + desc[i]+"\n");
+            }
+            Console.WriteLine("Total a pagar: R$" + total);
+            Console.ReadKey();
+            
 
         }
     }
