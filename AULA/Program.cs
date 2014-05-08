61dfb852b6df97cf2c7ecc754cfbbea5ada2ec49
Greicy de Matias Silva <greicy_matias@hotmail.com>
2014-05-08 10:13:29 -0300

    exercicios unidade VII

--- AULA/Program.cs
+++ AULA/Program.cs
@@ -21,5 +21,38 @@ namespace AULA
             //CASTING...
 
         }
+        static void Main(string[] args)
+        {
+            //quantidade de caracter que tem na string (contando os espaços);
+            string s = "Nome da Pessoa";
+            int quantidade = s.Length;
+            Console.WriteLine(quantidade);
+            
+            //recuperar um caracter de acordo com a sua posição
+            char c = s[0];
+            Console.WriteLine(c); //ixibira a letra 'N'
+
+            //verfica se tem aquela palavra/caracter na string
+
+            s = "Apostila de Java";
+
+            bool resultado1 = s.Contains("C#");
+            bool resultado2 = s.Contains("Java");
+
+            //false
+            Console.WriteLine(resultado1);
+
+            //true
+            Console.WriteLine(resultado2);
+
+
+            //Extrair um trecho de uma string
+            string s1 = " Thiago Sartor ";
+            string s2 = s1.Substring(7);    // " Sartor ";
+            string s3 = s1.Substring(0, 6); // " Thiag";
+            string s4 = s1.Substring(1, 1); // "T";
+
+        
+        }
     }
 }
--- Unidades/UnidadeVII.cs
+++ Unidades/UnidadeVII.cs
@@ -8,7 +8,7 @@ namespace Unidades
 {
     class UnidadeVII
     {
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
             int i = 0;
             int[] codigo = new int[15];
@@ -30,6 +30,53 @@ namespace Unidades
             Console.WriteLine("O maior preço foi do código {0}: R$ {1:F2}", codMaior, maior);
             Console.WriteLine("Média aritmética dos preços dos produtos: R$ {0:F2}", media);
             Console.ReadKey();
+        }
+        static void Main2(string[] args)
+        {
+            double salario = 0;
+            double salarioMaior = 0;
+            double mediaSalario = 0;
+            double mediaFilhos = 0;
+            int totalPessoas = 0;
+            int salarioMenor = 0;
+            double percentual = 0;
+            double filhos = 0;
+            do{
+                Console.Write("Digite o salário: ");
+                salario = double.Parse(Console.ReadLine());
+                if (salario < 0)
+                {
+                    Console.Clear();
+                    break;
+                }
+                salarioMaior = salario > salarioMaior ? salario : salarioMaior;
+                Console.Write("Digite a quantidade de filhos: ");
+                filhos = int.Parse(Console.ReadLine());
+                mediaSalario = salario > 0 ? mediaSalario + salario : mediaSalario;
+                totalPessoas = salario >= 0 ? totalPessoas + 1 : totalPessoas;
+                salarioMenor = salario < 150 && 0 <= salario ? salarioMenor + 1 : salarioMenor;
+                mediaFilhos += filhos;
+                Console.Clear();
+                Console.WriteLine("\t\t(Para terminar a coleta de dados digite um salário negativo!)\n");
+                Console.ReadKey();
+            }while(salario>=0);
+            if (totalPessoas > 0)
+            {
+                mediaSalario /= totalPessoas;
+                mediaFilhos /= totalPessoas;
+                percentual = (salarioMenor * 100) / totalPessoas;
+                Console.WriteLine("Média Salário da população: R$ {0:F2}", mediaSalario);
+                Console.WriteLine("Média Filhos da população: {0:F1}", mediaFilhos);
+                Console.WriteLine("Maior Sálario entre os habitantes: R$ {0:F2}", salarioMaior);
+                Console.WriteLine("Percentual de pessas com salário menor que R$ 150,00: {0:F2}%", percentual); ;
+                Console.ReadKey();
+            }
+        }
+        static void Main3(string[] args)
+        {
+            Console.WriteLine("Digite a quantidade total de mercadorias: ");
+            int total = int.Parse(Console.ReadLine());
+
         }
     }
 }
