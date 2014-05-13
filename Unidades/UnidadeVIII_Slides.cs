c90086cb37249c2e5e66d3067eccdde4a5dde5f7
Greicy de Matias Silva <greicy_matias@hotmail.com>
2014-05-13 11:49:38 -0300

    exercicios unidade VIII

--- Unidades/UnidadeVIII_Slides.cs
+++ Unidades/UnidadeVIII_Slides.cs
@@ -31,7 +31,7 @@ namespace Unidades
             Console.WriteLine("Preço: {0}; \nCor:  {1};\n{2}.",resultado, camisa,caminho);
             Console.ReadKey();
         }
-        static void Main(string[] args)
+        static void Main3(string[] args)
         {
             Random gerador = new Random();
             int A = gerador.Next(1, 5);
@@ -41,6 +41,59 @@ namespace Unidades
             resultado = (divisao == 0) ? "A DIVISIVEL POR B" : "A NÃO É DIVISIVEL POR B";
             Console.WriteLine(resultado);
             Console.ReadKey();
+        }
+
+        //INSTRUÇÕES REPETIÇÕES
+        static void Main4(string[] args)
+        {
+            //REPITA 5 VEZES;
+            int i = 0;
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Repita a mensagem...\n");
+                Console.ReadKey();                
+            }            
+        }
+        static void Main5(string[] args)
+        {
+            //IMPRIME 100
+            int i = 0;
+            for (i = 1; i < 101; i++)
+            {
+                Console.WriteLine(i);                
+            }
+            Console.ReadKey();
+        }
+        static void Main6(string[] args)
+        {
+            //IMPRIME 100, EXCETO MULTIPLOS DE 3
+            int i = 0;
+            for (i = 1; i < 101; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    continue;
+                }else
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+        static void Main7(string[] args)
+        {
+            //DIVIDE MAIOR INTEIRO
+            int num = 2147483647;
+            int i = 0;
+            int div = num;
+            for (i = 0; div > 100; i++)
+            {
+                div = div / 2;
+                Console.WriteLine(div+"\n");
+            }
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+
         }
     }
 }
