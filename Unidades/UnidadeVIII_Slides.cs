9d46b7d420fa66884923cac37981c209c52c6ef6
Greicy de Matias Silva <greicy_matias@hotmail.com>
2014-05-13 10:54:59 -0300

    exercicios slide unidade VIII

--- Unidades/UnidadeVIII_Slides.cs
+++ Unidades/UnidadeVIII_Slides.cs
@@ -18,12 +18,27 @@ namespace Unidades
             Console.WriteLine(resultado);
             Console.ReadKey();
         }
-        static void Main(string[] args)
+        static void Main2(string[] args)
         {
             Random gerador = new Random();
             double preco = gerador.NextDouble();
             string resultado = null;
+            string caminho = null;
             resultado = (preco >= 0.5) ? "CARO" : "BARATO";
+            string camisa = null;
+            camisa = (preco >= 0.5) ? "VERMELHA" : "AZUL";
+            caminho = (preco >= 0.5) ? "Vá para a direita" : "Vá para a esquerda";
+            Console.WriteLine("Preço: {0}; \nCor:  {1};\n{2}.",resultado, camisa,caminho);
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            Random gerador = new Random();
+            int A = gerador.Next(1, 5);
+            int B = gerador.Next(1, 5);
+            double divisao = A % B;
+            string resultado = null;
+            resultado = (divisao == 0) ? "A DIVISIVEL POR B" : "A NÃO É DIVISIVEL POR B";
             Console.WriteLine(resultado);
             Console.ReadKey();
         }
