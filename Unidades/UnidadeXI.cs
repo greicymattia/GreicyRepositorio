e332d4ac25e25536e30baaa9ef50f85473b6e2f6
GreicyMatias <greicy_matias@hotmail.com>
2014-06-03 09:59:46 -0300

    Unidade XI

---
+++ Unidades/UnidadeXI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidades
+{
+    class UnidadeXI
+    {
+        static int MaiorElemento(double[] array)
+        {
+            int maior = 0;            
+            for (int i = 1; i < array.Length; i++)
+            {
+                maior = (array[i] > array[maior]) ? i : maior;
+            }
+            return maior;
+        }
+        static int MenorElemento(double[] array)
+        {
+            int menor = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                menor = (array[i] < array[menor]) ? i : menor;
+            }
+                return menor;
+        }
+        static void Main(string[] args)
+        {
+            double[] vetor = new double[10];
+            Console.WriteLine("Digite 10 valores...");
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                vetor[i] = double.Parse(Console.ReadLine());
+            }
+            double Maior = MaiorElemento(vetor);
+            Console.WriteLine("Maior numero: {0}", Maior);
+            Console.ReadKey();
+
+        }
+    }
+}
