6f8f27c6ecc53dd8f43559323f0f15b4b771f003
Greicy de Matias Silva <greicy_matias@hotmail.com>
2014-06-09 11:43:27 -0300

    .

---
+++ Unidades/BandoDeDados/dbo.Aluno.data.sql
@@ -0,0 +1,7 @@
+﻿SET IDENTITY_INSERT [dbo].[Aluno] ON
+INSERT INTO [dbo].[Aluno] ([Id], [Nome], [Email], [Telefone], [Altura], [Aprovado]) VALUES (5, N'greicy', N'', N'', CAST( AS Decimal(3, 2)), NULL)
+INSERT INTO [dbo].[Aluno] ([Id], [Nome], [Email], [Telefone], [Altura], [Aprovado]) VALUES (6, N'mateus', N'', N'', CAST( AS Decimal(3, 2)), NULL)
+INSERT INTO [dbo].[Aluno] ([Id], [Nome], [Email], [Telefone], [Altura], [Aprovado]) VALUES (7, N'thiago', N'', N'', CAST( AS Decimal(3, 2)), NULL)
+INSERT INTO [dbo].[Aluno] ([Id], [Nome], [Email], [Telefone], [Altura], [Aprovado]) VALUES (8, N'lucas', N'', N'', CAST( AS Decimal(3, 2)), NULL)
+INSERT INTO [dbo].[Aluno] ([Id], [Nome], [Email], [Telefone], [Altura], [Aprovado]) VALUES (9, N'cgfgf', N'', N'', CAST( AS Decimal(3, 2)), 0)
+SET IDENTITY_INSERT [dbo].[Aluno] OFF
