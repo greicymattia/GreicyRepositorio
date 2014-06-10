deb93489f2761621fe8245f5aa01dfbe0dd14a14
Greicy de Matias Silva <greicy_matias@hotmail.com>
2014-06-10 11:50:32 -0300

    aula 10/06

---
+++ Unidades/BandoDeDados/Exercicio_6.sql
@@ -0,0 +1,7 @@
+﻿SELECT sum(altura)FROM Aluno -- soma das alturas
+SELECT sum(altura)FROM Aluno WHERE Aprovado = 0 -- soma das alturas dos alunos reprovados
+SELECT COUNT(*) FROM Aluno -- conta o total de registro
+SELECT MAX(Altura) FROM Aluno -- maior altura
+SELECT MIN(altura) FROM Aluno -- menor altura
+SELECT COUNT(*) as APROVADOS FROM Aluno WHERE Aprovado = 1 -- quantos aprovados
+-- AS APROVADOS EH UM APELIDO QUE SE DA A RESPOSTA.... AO INVEZ DE FICAR (NO COLUMN NAME)
